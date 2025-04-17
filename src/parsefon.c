33c6d887475d5a7cd2e0b334a66fa732f72c0fdd
robert <rjaremczak@gmail.com>
2025-04-17 21:48:09 +0200

    parsefon introduced

--- src/parsefon.c
+++ src/parsefon.c
@@ -1,5 +1,22 @@
+#include <stdio.h>
+
 #define PRG_NAME "parsefon"
 
+FILE *input_file;
+
 int main(int argc, char *argv[]) {
+    if (argc != 3)
+    {
+        printf("usage: " PRG_NAME "<input FON file> <output file prefix>\n");
+        return -1;
+    }
+
+    input_file = fopen(argv[1], "r");
+    if (!input_file)
+    {
+        printf(PRG_NAME ": input file %s could not be opened\n", argv[1]);
+        return -3;
+    }
+
     return 0;
 }
